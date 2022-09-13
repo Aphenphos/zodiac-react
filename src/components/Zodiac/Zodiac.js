bf2acd1135cb0dbe4d24978c91d62bcc0ab227cc
Aphenphos <MaxwillWinters@gmail.com>
2022-09-13 17:52:00 -0400

    added dynamic images

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -7,6 +7,7 @@ export default function Zodiac(props) {
       <span className="name">{props.sign}</span>
       <h1 className="sign-emoji">{props.emoji}</h1>
       <span>{props.dates}</span>
+      <img src={`${process.env.PUBLIC_URL}/signs/${props.sign}.png`}/>
     </div>
   );
 }
