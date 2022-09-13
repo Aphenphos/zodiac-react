eff3d9afc1e9808f2a709acba194b5261153558f
Aphenphos <MaxwillWinters@gmail.com>
2022-09-13 17:09:05 -0400

    sign data

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+
+
+export default function Main() {
+  return (
+    <main style={{ backgroundImage: "url('https://i.imgur.com/tREoxlU.jpeg')" }}>
+
+    </main>
+  );
+}
